741480b16650ddf33e90e562ee1035f37b1b43ea
xutao <429499381@qq.com>
2017-05-24 09:02:15 +0800

    webpack 2  无法正确 读取规则  changeText()

---
+++ react-dianping-getready-webpack 2/app/action/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by xutao on 2017/5/23.
+ */
+export default function changeText() {
+  type: 'CHANGE_TEXT'
+}
---
+++ react-dianping-getready-webpack 2/app/containers/Hello/Recommend/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by xutao on 2017/5/21.
+ */
+import React, { Component } from 'react'
+
+export default class extends Component {
+  render() {
+    return (
+      <p>Recomment</p>
+    )
+  }
+}
