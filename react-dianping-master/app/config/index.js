bd784110a6b3a0e72102eeef33a52bdcd0e9c193
xutao <429499381@qq.com>
2017-05-30 00:05:17 +0800

    mast 无法正确写入redux

---
+++ react-dianping-master/app/config/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by xutao on 2017/5/29.
+ */
+export const CITYNAME = 'USER_CURRENT_CITY_NAME'
--- react-dianping-master/app/containers/index.jsx
+++ react-dianping-master/app/containers/index.jsx
@@ -1,18 +1,58 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
+import  LocalStore from '../util/localStore'
+import { bindActionsCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as userInfoActonsxutao from '../actions/userinfo'
+
+import { CITYNAME } from '../config/index'
 
 class App extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = { initDone: false }
     }
     render() {
         return (
             <div>
-                {this.props.children}
+                {
+                    this.state.initDone
+                    ? this.props.children
+                    : <div>加载中...</div>
+                }
             </div>
         )
     }
+
+    componentDidMount() {
+      let cityName = LocalStore.getItem(CITYNAME)
+      if (cityName == null) {
+          cityName = '北京'
+      }
+
+      this.props.userInfoActions.update({
+        cityname: cityName,
+      })
+
+      this.setState({
+        initDone: true
+      })
+    }
 }
 
-export default App
+function mapStateToProps(state) {
+  return {
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+      userInfoActions: bindActionsCreators(userInfoActonsxutao, dispatch)
+  }
+}
+
+export default connect({
+  mapStateToProps,
+  mapDispatchToProps
+})(App)
