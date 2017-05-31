5c5ecd17181d7f033713af7d5393ee6660fcb7dd
xutao <429499381@qq.com>
2017-05-31 09:12:08 +0800

    无法正确 写入redux  cityName 没有报错

--- react-dianping-getready-redux-demo/app/reducers/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by xutao on 2017/5/28.
- */
-import { combineReducers } from 'redux'
-import userinfo from './userinfo'
-
-export default combineReducers({
-  userinfo
-})
--- react-dianping-getready-webpack/app/action/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by xutao on 2017/5/24.
- */
-
-export default function changeText() {
-  type: 'CHANGE_TEXT'
-}
